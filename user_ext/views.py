563556a78274a135327a8d71bd70d7dba29bc03d
miguel-pino20 <lucariodrago@gmail.com>
2025-01-14 17:55:44 -0500

    404

--- user_ext/views.py
+++ user_ext/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponsePermanentRedirect
 from django.shortcuts import render
 
 # Create your views here.
+# 404
+def not_found_view(request, exception=None):
+    if not request.path.endswith('/'):
+        return HttpResponsePermanentRedirect(request.path + '/')
+    return render(request, '404.html',context={'backend':'https://fastcv.myvnc.com/'}, status=404)
